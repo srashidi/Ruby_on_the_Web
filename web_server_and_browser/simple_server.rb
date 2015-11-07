58459dff4de5483f72372b31655228ad1d1d7a3b
Shaunty Rashidi <shauntyrashidi@Shaunty-Rashidis-Computer.local>
2015-11-06 16:02:30 -0800

    GET and POST requests work and input translated into HTML

--- web_server_and_browser/simple_server.rb
+++ web_server_and_browser/simple_server.rb
@@ -23,11 +23,17 @@ loop do
     request_array = initial_response_line.split(" ")
     method,path,version = request_array[0],request_array[1..-2].join(" "),request_array[-1]
     params = JSON.parse(path)
-    thanks_file = "web_server_and_browser/thanks_viking.html"
+    thanks_file = "web_server_and_browser/thanks.html"
+    lines = File.open(thanks_file).readlines.each_with_index do |line,index|
+      if line.include?("<%= yield %>")
+        @replacement = index
+      end
+    end
+    lines[@replacement] = "<li>Name: #{params["viking"]["name"]}</li><li>Email: #{params["viking"]["email"]} </li>\n"
     client.puts(version + " 200 OK")
     client.puts("Date: " + Time.now.ctime)
     client.puts("Content Length: " + File.open(thanks_file).size.to_s + "\r\n\r\n")
-    client.puts(File.open(thanks_file).readlines.each { |line| puts line })
+    client.puts(lines)
   end
   client.puts "Closing the connection. Bye!"
   client.close
